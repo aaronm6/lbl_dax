e8874b811507b5f1d34488647f97ff01b29bbba6
Aaron M <aaronm6@gmail.com>
2025-06-23 14:40:47 -0700

    Forgot to increment the version

--- data_proc/ldax_processing/setup.py
+++ data_proc/ldax_processing/setup.py
@@ -10,7 +10,7 @@ module = Extension(
 #print(find_packages(include=["ldax_processing"]))
 setup(
     name="ldax_processing",
-    version="0.0.1",
+    version="0.0.2",
     description="Data-processing library for ldax",
     author="Aaron Manalaysay",
     license="GPL-2.1",
