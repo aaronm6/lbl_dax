750f03b965b5518640f8ac81c95409988b841cea
Aaron M. <aaronm6@gmail.com>
2025-10-23 16:54:26 -0700

    fixed a file handle misspelling

--- aaron/DDC40_BinReader.py
+++ aaron/DDC40_BinReader.py
@@ -120,7 +120,7 @@ def Read_DDC40_fHandle(fp, start_event=0, num_events=-1):
         ch_hit_vector[k], = freader(fp, dtype=np.uint64, count=1)
         
         waveforms[k,...] = freader(
-            fq, 
+            fp, 
             dtype=np.int16, 
             count=waveInfo['num_channels']*waveInfo['num_samples']
         ).reshape(waveInfo['num_channels'],waveinfo['num_samples'])
