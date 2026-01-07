200b31d70de832ab39111382aedbf90b4ca02313
Aaron ldax <aaronm@lbl.gov>
2026-01-07 14:22:46 -0800

    Added job handler for processing jobs

---
+++ data_proc/ldax_scripts/launch_proc_jobs.py
@@ -0,0 +1,50 @@
+import os, subprocess
+from time import sleep
+
+raw_data_path = '/mnt/drive1/TPC_data'
+rq_path = '/mnt/drive2/TPC_RQs'
+#log_path = 
+
+def file_tags(filename_list, extensions):
+    if isinstance(extensions, str):
+        extensions = (extensions,)
+    file_tags = []
+    for fname in filename_list:
+        fname_list = fname.split('.')
+        file_tag_list = [item for item in fname_list if item not in extensions]
+        file_tags.append('.'.join(file_tag_list))
+    return file_tags
+
+def main():
+    raw_files = os.listdir(raw_data_path)
+    rq_files = os.listdir(rq_path)
+    
+    file_raw_tag = file_tags(raw_files, ('bin','gz'))
+    file_rq_tags = file_tags(rq_files, ('vrz',))
+    
+    files_to_process = [item for item in file_raw_tag if item not in file_rq_tags]
+    files_in_process = []
+    files_processed = []
+    
+    files_in_process_dict = {}
+    
+    while files_to_process or files_in_process:
+        while (len(files_in_process)<4) and files_to_process:
+            file_proc = files_to_process.pop()
+            print(f"--Processing {file_proc}", flush=True)
+            p = subprocess.Popen(['python','ldax_proc_script.py',f'{file_proc}.bin.gz'])
+            files_in_process.append(file_proc)
+            files_in_process_dict[file_proc] = p
+        # check for processes finished
+        for file in files_in_process:
+            if files_in_process_dict[file].poll() is not None:
+                print(f"--finished {file}", flush=True)
+                files_processed.append(file)
+                files_in_process.remove(file)
+                del files_in_process_dict[file]
+        sleep(10)
+    print('********* finished ******')
+
+if __name__ == "__main__":
+    main()
+
