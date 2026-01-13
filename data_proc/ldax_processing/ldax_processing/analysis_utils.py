09ef1bc96c90977382c84b8b59c23b491bfcaa3c
Aaron M <aaronm6@gmail.com>
2026-01-13 15:32:19 -0800

    Made circle-plotting (of TPC diameter) optional

--- data_proc/ldax_processing/ldax_processing/analysis_utils.py
+++ data_proc/ldax_processing/ldax_processing/analysis_utils.py
@@ -27,17 +27,26 @@ for k4 in range(4):
 a_ch = 6.15 # mm
 
 ch_patches = [Rectangle(tuple(ch_corner_pos_mm[k,:]), a_ch, a_ch) for k in range(16)]
-
 tpc_circle = Circle((0., 0.), radius=15., facecolor='none', edgecolor='r',lw=.5)
 
-def plot_top_pattern(hit_i, ax=None, cmap=None):
+def plot_top_pattern(hit_i, ax=None, cmap=None, add_circ=True):
+    """
+    plot_top_pattern
+    
+    Take an array of 16 hit values (one for each SiPM in an array), plot the hit pattern
+    with boxes shaded with a color scale.  Input `hit_i` should be a 1d, length-16 array.
+    
+    Optionally, this script can plot a circle the same size as the diameter of the TPC
+    i.e. 30mm, with keyword argument 'add_circ'. (default is True).
+    """
     if ax is None:
         ax = plt.gca()
     hit_collection = PatchCollection(ch_patches, array=np.asarray(hit_i).squeeze(), cmap=cmap)
     ax.add_collection(hit_collection)
     plt.sci(hit_collection)
     ax.autoscale_view()
-    ax.add_patch(tpc_circle)
+    if add_circ:
+        ax.add_patch(tpc_circle)
     return hit_collection
 
 
