ffc9c93fa336bc7bca354a32aca46183c3435d4c
TarZBlamm <tarzanzhu25@gmail.com>
2026-02-12 10:29:05 -0800

    Merge request for list based processing option

--- data_proc/ldax_scripts/launch_proc_jobs_list.py
+++ data_proc/ldax_scripts/launch_proc_jobs_list.py
@@ -25,7 +25,7 @@ def parse_some_args():
         default=default_rq_path, help="The path in which to search for and save RQ data")
     parser.add_argument('-f', action='store', dest='file_list',
         help="List of files to be processed")
-    parser.add_argument('--conf', action='store', dest='conf_file', 
+    parser.add_argument('--conf', action='store', dest='conf_file',
         default='proc_settings_v001.yaml', help="Settings file for processing jobs")
     args = parser.parse_args()
     return args
